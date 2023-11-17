573f81ee9a9f55aad041171de771185e08bfe19d
luskas8 <gnomo1049@gmail.com>
2023-11-16 22:30:47 -0300

    feat: font

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,13 +2,32 @@ import { ImageBackground, StyleSheet, View } from 'react-native'
 
 import backgroundImage from '../assets/images/diocese_icon.png'
 import { StatusBarProvider } from '../contexts/StatusBar'
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 // eslint-disable-next-line react/prop-types
 export default function DefaultLayout ({ children }) {
+  const [fontsLoaded] = useFonts({
+    Montserrat: require('../assets/fonts/Montserrat-Regular.otf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <StatusBarProvider style={styles.container}>
       <ImageBackground source={backgroundImage} style={styles.image} resizeMode='contain' />
-      <View style={styles.children}>
+      <View style={styles.children} onLayout={onLayoutRootView}>
         {children}
       </View>
     </StatusBarProvider>
