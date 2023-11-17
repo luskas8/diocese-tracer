26d110ed460ae6754ae5e3fb5e067a29b37d4122
luskas8 <gnomo1049@gmail.com>
2023-11-16 22:31:10 -0300

    feat: home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,41 +1,73 @@
-import { Pressable, StyleSheet, Text, View, SectionList } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import DefaultLayout from '../layouts/default'
 
 export default function MainPanel ({ navigation: { navigate } }) {
   return (
     <DefaultLayout>
       <View style={styles.container}>
-        <Text>Página inicial</Text>
-        <SectionList
-          style={{
-            marginTop: 15,
-            flexGrow: 0
-          }}
-          ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-          sections={[
-            {
-              data: [
-                { page: 'Mapa', text: 'Go to Mapa' },
-                { page: 'Tutorial', text: 'Go to Tutorial' }
-              ]
-            }
-          ]}
-          renderItem={({ item }) => (
-            <Pressable style={styles.button} onPress={() => navigate(item.page)}>
-              <Text style={styles.text}>{item.text}</Text>
-            </Pressable>
-          )}
-        />
+        <View>
+          <Text style={styles.title}>Encontre a paróquia mais próxima!</Text>
+          <Text style={styles.subtitle}>Você pode achar todas as paróquias da Baixada Santista</Text>
+        </View>
+        <View style={styles.localizationView}>
+          <Pressable style={styles.button} onPress={() => {}}>
+            <Text style={{ ...styles.text, textTransform: 'uppercase' }}>localizar-me</Text>
+          </Pressable>
+          <View>
+            <Text style={styles.middleView}>ou</Text>
+          </View>
+          <TextInput
+            style={styles.inputText}
+            placeholder='Digite um local'
+          />
+          <Picker>
+            <Picker.Item label='Selecione uma cidade' value='' />
+            <Picker.Item label='Santos' value='santos' />
+            <Picker.Item label='São Vicente' value='sao-vicente' />
+            <Picker.Item label='Praia Grande' value='praia-grande' />
+            <Picker.Item label='Guarujá' value='guaruja' />
+            <Picker.Item label='Cubatão' value='cubatao' />
+            <Picker.Item label='Bertioga' value='bertioga' />
+            <Picker.Item label='Peruíbe' value='peruibe' />
+            <Picker.Item label='Itanhaém' value='itanhaem' />
+            <Picker.Item label='Mongaguá' value='mongagua' />
+            <Picker.Item label='Itariri' value='itariri' />
+            <Picker.Item label='Pedro de Toledo' value='pedro-de-toledo' />
+            <Picker.Item label='Miracatu' value='miracatu' />
+            <Picker.Item label='Juquiá' value='juquia' />
+            <Picker.Item label='Eldorado' value='eldorado' />
+            <Picker.Item label='Iguape' value='iguape' />
+            <Picker.Item label='Ilha Comprida' value='ilha-comprida' />
+            <Picker.Item label='Cananéia' value='cananeia' />
+            <Picker.Item label='Registro' value='registro' />
+            <Picker.Item label='Jacupiranga' value='jacupiranga' />
+            <Picker.Item label='Cajati' value='cajati' />
+          </Picker>
+        </View>
+        <Pressable style={styles.hint} onPress={() => navigate('Tutorial')}>
+          <Text style={styles.text}>Dúvidas</Text>
+        </Pressable>
+        {/* <View style={styles.hintView}>
+        </View> */}
       </View>
     </DefaultLayout>
   )
 };
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white'
+  },
   container: {
+    paddingVertical: 22,
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between'
   },
   button: {
     alignItems: 'center',
@@ -46,11 +78,61 @@ const styles = StyleSheet.create({
     elevation: 3,
     backgroundColor: '#3F88EF'
   },
-  text: {
+  title: {
+    paddingHorizontal: 16,
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Montserrat',
+    fontSize: 28,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 30,
+    letterSpacing: 0.84
+  },
+  subtitle: {
+    paddingHorizontal: 8,
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Montserrat',
+    fontSize: 12,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 25//,
+    // letterSpacing: -0.24
+  },
+  localizationView: {
+    paddingVertical: 32,
+    paddingHorizontal: 40,
+    backgroundColor: 'rgba(101, 158, 237, 0.60)',
+    borderRadius: 2
+  },
+  middleView: {
+    borderRadius: 100,
+    backgroundColor: '#616D7C',
+    marginVertical: 24,
+    width: 25,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '300',
+    color: '#FFF'
+  },
+  inputText: {
+    backgroundColor: '#FFF',
+    flexShrink: 0,
+    color: '#757575',
+    fontFamily: 'Montserrat',
     fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white'
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 24
+  },
+  hint: {
+    marginTop: 15,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    elevation: 3,
+    backgroundColor: '#3F88EF'
   }
 })
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack'
 
+import { Image, Pressable, StyleSheet } from 'react-native'
 import HomePage from './home'
 import MapPage from './map'
 import TutorialPage from './tutorial'
@@ -7,15 +8,50 @@ import TutorialPage from './tutorial'
 const Stack = createStackNavigator()
 
 export function MainStack () {
+  const headerOptions = {
+    headerTitleStyle: styles.title,
+    headerStyle: styles.header,
+    headerTitle: 'Diocese de Santos',
+    headerShadowVisible: false
+  }
+
   return (
     <Stack.Navigator initialRouteName='Home'>
-      <Stack.Screen name="Home" component={HomePage} options={{ header: StackHeader }} />
-      <Stack.Screen name="Tutorial" component={TutorialPage} />
+      <Stack.Screen name="Home" component={HomePage} options={{ ...headerOptions, headerLeft: HomeButoon }} />
+      <Stack.Screen name="Tutorial" component={TutorialPage} options={{ ...headerOptions, headerLeft: HomeButoon }} />
       <Stack.Screen name="Mapa" component={MapPage} />
     </Stack.Navigator>
   )
 }
 
-function StackHeader () {
-  return null
+function HomeButoon (props) {
+  return <Pressable style={styles.backButtom} onPress={props.onPress}>
+    <Image source={require('../assets/images/diocese_icon.png')} style={styles.icon} />
+  </Pressable>
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#367BC1'
+  },
+  backButtom: {
+    width: 60,
+    height: 60,
+    padding: 8,
+    flex: 1,
+    justifyContent: 'center'
+  },
+  icon: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center'
+  },
+  title: {
+    color: '#FFF',
+    fontFamily: 'Montserrat',
+    fontSize: 18,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 60
+  }
+})
