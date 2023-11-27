733c5f36f160968a2872cb482a80db4a23e35e35
luskas8 <gnomo1049@gmail.com>
2023-11-27 11:22:07 -0300

    feat: added maps direction

--- src/components/BottonPopup.js
+++ src/components/BottonPopup.js
@@ -1,8 +1,10 @@
 import BottomSheet from '@gorhom/bottom-sheet'
-import React, { useMemo, useRef } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import { getApps } from 'react-native-map-link'
 
 const BottonPopup = ({ children, chunch }) => {
+  const [availableApps, updateAvailableApps] = useState([])
   // ref
   const bottomSheetRef = useRef(null)
 
@@ -14,6 +16,22 @@ const BottonPopup = ({ children, chunch }) => {
   //   if (!chunch) return
   //   console.log('handleSheetChanges', index)
   // }, [])
+
+  useEffect(() => {
+    if (!chunch) {
+      return
+    }
+
+    (async () => {
+      const apps = await getApps({
+        latitude: chunch.geometry.coordinates[1],
+        longitude: chunch.geometry.coordinates[0],
+        title: chunch.name,
+        googleForceLatLon: true
+      })
+      updateAvailableApps(apps)
+    })()
+  }, [chunch])
 
   // renders
   return (
@@ -32,6 +50,15 @@ const BottonPopup = ({ children, chunch }) => {
           {chunch && (
             <View>
               <Text style={styles.name}>{chunch.name} - {chunch.distance}</Text>
+              <Text style={styles.itemTitle}>Como chegar:</Text>
+              <View style={styles.directionContainer}>
+                {availableApps.map(({ icon, name, id, open }) => (
+                  <Pressable style={styles.direction} key={id} onPress={open}>
+                    <Image style={styles.icon} source={icon} />
+                    <Text style={{ ...styles.item, marginTop: 5 }}>{name}</Text>
+                  </Pressable>
+                ))}
+              </View>
               <Text style={styles.itemTitle}>Horários:</Text>
               {chunch.description.horario.map((horario) => <Text style={styles.item} key={horario}>{horario}</Text>)}
               <Text style={{ ...styles.itemTitle, marginTop: 16 }} >Endereço:</Text>
@@ -76,6 +103,20 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     fontStyle: 'normal',
     lineHeight: 30
+  },
+  icon: {
+    width: 30,
+    height: 30
+  },
+  direction: {
+    alignItems: 'center',
+    paddingVertical: 6
+  },
+  directionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 6
   }
 })
 
